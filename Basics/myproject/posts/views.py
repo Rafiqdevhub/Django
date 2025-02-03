d89058e73a174bb07e1ac27ef57617c5c0d47854
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-03 09:30:19 +0500

    Update post templates and URLs for consistency; add banner image to post page and correct URL names

--- Basics/myproject/posts/views.py
+++ Basics/myproject/posts/views.py
@@ -7,6 +7,6 @@ def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
-def posts_page(request, slug):
+def post_page(request, slug):
     post = Post.objects.get(slug=slug)
-    return render(request, 'posts/posts_page.html', {'post': post})
+    return render(request, 'posts/post_page.html', {'post': post})
